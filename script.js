a73dd97c9f03ecb9b24d768b362fdcf00b3b2713
vramsarran <98958631+vramsarran@users.noreply.github.com>
2022-06-03 13:48:41 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,72 @@
+$(".contact").hide();
+$(".aircraft").hide();
+
+$(".btn-bgstroke").click(function() {
+    $(".header").show();
+    $(".info").show();
+    $(".images").show();
+    $(".title1").show();
+    $(".title").show();
+    $(".work").show();
+    $(".work2").show();
+    $(".work3").show();
+    $(".work4").show();
+    $(".short").show();
+    $(".contact").hide();
+    $(".aircraft").hide();
+
+});
+
+$(".btn-bgstroke2").click(function() {
+    $(".header").show();
+    $(".info").hide();
+    $(".title").hide();
+    $(".title1").hide();
+    $(".images").hide();
+    $(".work").hide();
+    $(".work2").hide();
+    $(".work3").hide();
+    $(".work4").hide();
+    $(".short").hide();
+    $(".aircraft").show();
+
+
+});
+
+$(".btn-bgstroke3").click(function() {
+    $(".header").show();
+    $(".info").hide();
+    $(".title").hide();
+    $(".title1").hide();
+    $(".images").hide();
+    $(".work").hide();
+    $(".work2").hide();
+    $(".work3").hide();
+    $(".work4").hide();
+    $(".short").hide();
+    $(".aircraft").hide();
+
+
+
+
+});
+
+$(".btn-bgstroke4").click(function() {
+    $(".header").show();
+    $(".info").hide();
+    $(".title").hide();
+    $(".title1").hide();
+    $(".images").hide();
+    $(".work").hide();
+    $(".work2").hide();
+    $(".work3").hide();
+    $(".work4").hide();
+    $(".short").hide();
+    $(".contact").show();
+    $(".infor").hide();
+    $(".aircraft").hide();
+
+
+
+
+});
